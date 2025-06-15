6b94e716c008dab82c257e840d641ca4676f9166
thiagoshun <shun.thiago@gmail.com>
2025-06-15 17:11:17 +1000

    dados main

--- main.py
+++ main.py
@@ -1,2 +1,37 @@
 
+import data
+import helpers
+
 class TestUrbanRoutes:
+    def test_set_route("self"):
+        #Adicionar em S8
+        print("função criada para definir a rota")
+        pass
+    def test_select_plan("self"):
+        # Adicionar em S8
+        print("função criada para escolher o plano")
+        pass
+    def test_fill_phone_number("self"):
+        # Adicionar em S8
+        print("função criada para colocar o telefone")
+        pass
+    def test_fill_card("self"):
+        # Adicionar em S8
+        print("função criada dados do cartão")
+        pass
+    def test_comment_for_driver("self"):
+        # Adicionar em S8
+        print("função criada papara comentário")
+        pass
+    def test_order_blanket_and_handkerchiefs("self"):
+        # Adicionar em S8
+        print("função criada")
+        pass
+    def est_order_2_ice_creams("self"):
+        # Adicionar em S8
+        print("função criada para dados do sorvete")
+        pass
+    def test_car_search_model_appears("self"):
+        # Adicionar em S8
+        print("função criada")
+        pass
