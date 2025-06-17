bf2fe87d016d8656335ebe1a0fadef130efcf447
thiagoshun <shun.thiago@gmail.com>
2025-06-17 14:27:30 +1000

    repository done

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 # Arquivo com constantes
-URBAN_ROUTES_URL = ''
+URBAN_ROUTES_URL = 'https://cnt-2e0ccbf8-2b5b-4b11-b7ee-ddebd8cc4f4a.containerhub.tripleten-services.com?lng=pt'
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO	= '1300 1st St'
 PHONE_NUMBER	= '+1 123 123 12 12'
--- main.py
+++ main.py
@@ -2,36 +2,53 @@
 import data
 import helpers
 
+
 class TestUrbanRoutes:
-    def test_set_route("self"):
-        #Adicionar em S8
+    @classmethod
+    def setup_class(cls):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Conectado ao servidor Urban Routes")
+        else:
+            print("Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução")
+
+    def test_set_route(self):
+        # Adicionar em S8
         print("função criada para definir a rota")
         pass
-    def test_select_plan("self"):
+
+    def test_select_plan(self):
         # Adicionar em S8
         print("função criada para escolher o plano")
         pass
-    def test_fill_phone_number("self"):
+
+    def test_fill_phone_number(self):
         # Adicionar em S8
         print("função criada para colocar o telefone")
         pass
-    def test_fill_card("self"):
+
+    def test_fill_card(self):
         # Adicionar em S8
         print("função criada dados do cartão")
         pass
-    def test_comment_for_driver("self"):
+
+    def test_comment_for_driver(self):
         # Adicionar em S8
-        print("função criada papara comentário")
+        print("função criada para comentário")
         pass
-    def test_order_blanket_and_handkerchiefs("self"):
+
+    def test_order_blanket_and_handkerchiefs(self):
         # Adicionar em S8
         print("função criada")
         pass
-    def test_order_2_ice_creams("self"):
+
+    def test_order_2_ice_creams(self):
         # Adicionar em S8
-        print("função criada para dados do sorvete")
+        number_of_ice_creams = 2
+        for count in range(number_of_ice_creams):
+            print("função criada para definir o tipo sorvete")
         pass
-    def test_car_search_model_appears("self"):
+
+    def test_car_search_model_appears(self):
         # Adicionar em S8
         print("função criada")
         pass
